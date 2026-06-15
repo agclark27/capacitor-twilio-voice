a58eea6ebd23c5b14b37408b35c10f6a9902c762
Martin Donadieu <martindonadieu@gmail.com>
2026-06-15 23:59:17 +0200

    fix: address Capgo example deploy review

--- example-app/ios/App/CapApp-SPM/Package.swift
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -11,11 +11,13 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
-        .package(name: "CapacitorLocalNotifications", path: "../../../node_modules/.bun/@capacitor+local-notifications@7.0.3+15e98482558ccfe6/node_modules/@capacitor/local-notifications"),
-        .package(name: "CapacitorPushNotifications", path: "../../../node_modules/.bun/@capacitor+push-notifications@7.0.3+15e98482558ccfe6/node_modules/@capacitor/push-notifications"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
+        .package(name: "CapacitorLocalNotifications", path: "../../../node_modules/.bun/@capacitor+local-notifications@8.2.0+15e98482558ccfe6/node_modules/@capacitor/local-notifications"),
+        .package(name: "CapacitorPushNotifications", path: "../../../node_modules/.bun/@capacitor+push-notifications@8.1.1+15e98482558ccfe6/node_modules/@capacitor/push-notifications"),
         .package(name: "CapgoCapacitorTwilioVoice", path: "../../../node_modules/.bun/@capgo+capacitor-twilio-voice@file+..+8ad980643f635cc1/node_modules/@capgo/capacitor-twilio-voice"),
-        .package(name: "CapgoNativeAudio", path: "../../../node_modules/.bun/@capgo+native-audio@7.6.1+15e98482558ccfe6/node_modules/@capgo/native-audio")
+        .package(name: "CapgoNativeAudio", path: "../../../node_modules/.bun/@capgo+native-audio@8.4.2+15e98482558ccfe6/node_modules/@capgo/native-audio"),
+        .package(name: "CapgoCapacitorUpdater", path: "../../../node_modules/.bun/@capgo+capacitor-updater@8.47.10+15e98482558ccfe6/node_modules/@capgo/capacitor-updater"),
+        .package(name: "CapacitorSplashScreen", path: "../../../node_modules/.bun/@capacitor+splash-screen@8.0.1+15e98482558ccfe6/node_modules/@capacitor/splash-screen")
     ],
     targets: [
         .target(
@@ -26,7 +28,9 @@ let package = Package(
                 .product(name: "CapacitorLocalNotifications", package: "CapacitorLocalNotifications"),
                 .product(name: "CapacitorPushNotifications", package: "CapacitorPushNotifications"),
                 .product(name: "CapgoCapacitorTwilioVoice", package: "CapgoCapacitorTwilioVoice"),
-                .product(name: "CapgoNativeAudio", package: "CapgoNativeAudio")
+                .product(name: "CapgoNativeAudio", package: "CapgoNativeAudio"),
+                .product(name: "CapgoCapacitorUpdater", package: "CapgoCapacitorUpdater"),
+                .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen")
             ]
         )
     ]
